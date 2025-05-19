d5ea80d2e2279c215ce2d34240a146acade20645
unknown <aydansb@code.edu.az>
2025-05-19 09:32:50 -0800

    Add responsiveness to Preloader effect and header sections

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -8,4 +8,38 @@
 	$window.on('load', function(){
 		$(".preloader").fadeOut(600);
 	});
+
+
+    
+	/* Sticky Header */	
+	if($('.active-sticky-header').length){
+		$window.on('resize', function(){
+			setHeaderHeight();
+		});
+
+		function setHeaderHeight(){
+	 		$("header.main-header").css("height", $('header .header-sticky').outerHeight());
+		}	
+	
+		$window.on("scroll", function() {
+			var fromTop = $(window).scrollTop();
+			setHeaderHeight();
+			var headerHeight = $('header .header-sticky').outerHeight()
+			$("header .header-sticky").toggleClass("hide", (fromTop > headerHeight + 100));
+			$("header .header-sticky").toggleClass("active", (fromTop > 600));
+		});
+	}	
+	
+	/* Slick Menu JS */
+	$('#menu').slicknav({
+		label : '',
+		prependTo : '.responsive-menu'
+	});
+
+	if($("a[href='#top']").length){
+		$(document).on("click", "a[href='#top']", function() {
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+			return false;
+		});
+	}
 })(jQuery);
