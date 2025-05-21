93f9291f391e9123606de8ad740d9bbe98e86a30
unknown <aydansb@code.edu.az>
2025-05-21 02:02:43 -0800

    wrote a new section

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,37 +1,80 @@
 "use strict";
 
+function handlePreloader() {
+    var $preloader = $(".preloader");
+
+    $(window).on("load", function () {
+        setTimeout(function () {
+            $preloader.fadeOut(600);
+        }, 3000);
+    });
+
+    setTimeout(function () {
+        if ($preloader.is(":visible")) {
+            $preloader.fadeOut(600);
+        }
+    }, 1000);
+}
+
+$(document).ready(function () {
+    handlePreloader();
+});
+
+
+function initStickyHeader() {
+	if ($('.active-sticky-header').length) {
+		const $window = $(window);
+
+		function setHeaderHeight() {
+			$("header.main-header").css("height", $('header .header-sticky').outerHeight());
+		}
+
+		$window.on('resize', function () {
+			setHeaderHeight();
+		});
+
+		
+		$window.on("scroll", function () {
+			var fromTop = $window.scrollTop();
+			setHeaderHeight();
+			var headerHeight = $('header .header-sticky').outerHeight();
+
+			$("header .header-sticky").toggleClass("hide", (fromTop > headerHeight + 100));
+			$("header .header-sticky").toggleClass("active", (fromTop > 600));
+		});
+	}
+}
+
+$(document).ready(function () {
+	initStickyHeader();
+});
+
+
+function initMobileMenu(menuSelector, targetSelector, labelText = '') {
+	$(menuSelector).slicknav({
+		label: labelText,
+		prependTo: targetSelector
+	});
+}
+
+$(document).ready(function () {
+
+	initMobileMenu('#menu', '.responsive-menu'); 
+});
+
+
 (function ($) {
    
 	
 	var $window = $(window); 
 	var $body = $('body'); 
-
-	$window.on('load', function(){
-		$(".preloader").fadeOut(600);
-	});
-
-	if($('.active-sticky-header').length){
-		$window.on('resize', function(){
-			setHeaderHeight();
-		});
-
-		function setHeaderHeight(){
-	 		$("header.main-header").css("height", $('header .header-sticky').outerHeight());
-		}	
-	
-		$window.on("scroll", function() {
-			var fromTop = $(window).scrollTop();
-			setHeaderHeight();
-			var headerHeight = $('header .header-sticky').outerHeight()
-			$("header .header-sticky").toggleClass("hide", (fromTop > headerHeight + 100));
-			$("header .header-sticky").toggleClass("active", (fromTop > 600));
-		});
-	}	
-	
-	$('#menu').slicknav({
-		label : '',
-		prependTo : '.responsive-menu'
-	});
+    
+    $('#menu').slicknav({
+        label: '',
+        prependTo: '.responsive-menu', // hamburger menyusu hara yerləşdiriləcək
+        closedSymbol: '&#9658;',       // ▶
+        openedSymbol: '&#9660;'        // ▼
+      });
 
 	if($("a[href='#top']").length){
 		$(document).on("click", "a[href='#top']", function() {
